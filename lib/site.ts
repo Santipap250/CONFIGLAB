05a353cc68f6b939edd93d1684b439f83ace7d94
TUIzFPV <santipap350z@gmail.com>
2026-08-05 13:08:47 +0700

    Update SITE_URL logic in site.ts
    Refactor SITE_URL assignment to include a default value.

--- lib/site.ts
+++ lib/site.ts
@@ -1,10 +1,8 @@
-// lib/site.ts
 const rawHost =
   process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ||
-  process.env.NEXT_PUBLIC_SITE_URL;
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  "obixconfiglab.vercel.app";
 
-export const SITE_URL = rawHost
-  ? rawHost.startsWith("http")
-    ? rawHost
-    : `https://${rawHost}`
-  : "http://localhost:3000";
+export const SITE_URL = rawHost.startsWith("http")
+  ? rawHost
+  : `https://${rawHost}`;
